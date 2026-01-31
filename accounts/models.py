e56d9f7c17e629988494d0cd9b72327ab10b742a
owais6824 <mawais6824@gmail.com>
2026-01-31 18:35:04 +0500

    Initial backend setup: users, clinics, doctors, appointments

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('SUPER_ADMIN' , 'Super Admin'),
+        ('CLINIC_ADMIN' , 'Clinic Admin'),
+        ('DOCTOR' , 'Doctor'),
+        ('RECEPTIONIST' , 'Receptionist'),
+        ('PATIENT' , 'Patient'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    def __str__(self):
+        return f"{(self.username)} ({self.role})"
